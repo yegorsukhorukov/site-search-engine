eaab44832375fb90c909e9f90fa67feb8111aa25
Yegor Sukhorukov <egorsukhorukov@mail.ru>
2022-06-09 22:29:47 +0300

    adding indexes to the index table

--- src/main/java/ru/yegor/siteSearchEngine/WebPageIndexingSystem.java
+++ src/main/java/ru/yegor/siteSearchEngine/WebPageIndexingSystem.java
@@ -1,10 +1,13 @@
 package ru.yegor.siteSearchEngine;
 
 import org.jsoup.Connection;
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.yegor.siteSearchEngine.lemmatizer.ConvertingTextToLemmas;
+import ru.yegor.siteSearchEngine.model.Lemma;
 import ru.yegor.siteSearchEngine.model.Page;
 
 import java.io.IOException;
@@ -16,9 +19,10 @@ import java.util.*;
 import java.util.concurrent.RecursiveTask;
 
 public class WebPageIndexingSystem extends RecursiveTask<Set<Page>> {
-    private static final Set<String> uniqueUrls = new TreeSet<>(); //коллекция уникальных ссылок
+    private static final Set<String> uniqueUrls = new TreeSet<>();
+    private static final Set<String> uniqueLemmas = new TreeSet<>();
 
-    private final String[] extensions = {".pdf", ".png", ".jpeg", ".jpg"}; //массив расширений файлов, ссылки на которые не будут включены в итоговую карту сайта
+    private final String[] extensions = {".pdf", ".png", ".jpeg", ".jpg"};
 
     private final String urlSite;
     private final String url;
@@ -51,9 +55,29 @@ public class WebPageIndexingSystem extends RecursiveTask<Set<Page>> {
 
             Document document = response.parse();
 
+            String titleText = getTextFromTitleOfPage(document);
+            String bodyText = getTextFromBodyOfPage(document);
+
+            ConvertingTextToLemmas convertingTextToLemmas = new ConvertingTextToLemmas();
+            Set<Lemma> titleLemmas = convertingTextToLemmas.getLemmas(titleText);
+            Set<Lemma> bodyLemmas = convertingTextToLemmas.getLemmas(bodyText);
+
+            insertIntoTableLemma(titleLemmas);
+            insertIntoTableLemma(bodyLemmas);
+
             Page page = new Page(getUrl().substring(getUrlSite().length() - 1), response.statusCode(), document.toString());
             insertIntoTablePage(page);
             pages.add(page);
+
+            for (Lemma lemma : titleLemmas) {
+                insertIntoTableIndex(page, lemma);
+            }
+
+            for (Lemma lemma : bodyLemmas) {
+                if (getIndexIdFromTableIndex(page, lemma) > 0) {
+                    updateRankInTableIndex(page, lemma);
+                } else insertIntoTableIndex(page, lemma);
+            }
 
             Elements elements = document.select("a");
 
@@ -75,10 +99,241 @@ public class WebPageIndexingSystem extends RecursiveTask<Set<Page>> {
             for (WebPageIndexingSystem task : tasks) {
                 pages.addAll(task.join());
             }
+        } catch (HttpStatusException e) {
+            insertIntoTablePage(new Page(e.getUrl().substring(getUrlSite().length() - 1), e.getStatusCode(), ""));
+            pages.add(new Page(e.getUrl().substring(getUrlSite().length() - 1), e.getStatusCode(), ""));
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
         return pages;
+    }
+
+    private void insertIntoTableIndex(Page page, Lemma lemma) {
+        java.sql.Connection connection = ConnectionManager.getConnection();
+        String insertQuery = "INSERT INTO search_engine.index (page_id, lemma_id, `rank`) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+            preparedStatement.setInt(1, getPageIdFromTablePage(page));
+            preparedStatement.setInt(2, getLemmaIdFromTableLemma(lemma));
+            preparedStatement.setFloat(3, (float) 1.0 * lemma.getFrequency());
+            preparedStatement.executeUpdate();
+
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int getPageIdFromTablePage(Page page) {
+        java.sql.Connection connection = ConnectionManager.getConnection();
+        String checkQuery = "SELECT id FROM search_engine.page WHERE path = ?";
+        int pageId = 0;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(checkQuery);
+            preparedStatement.setString(1, page.getPath());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                pageId = resultSet.getInt("id");
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return pageId;
+    }
+
+    private int getLemmaIdFromTableLemma(Lemma lemma) {
+        java.sql.Connection connection = ConnectionManager.getConnection();
+        String checkQuery = "SELECT id FROM search_engine.lemma WHERE lemma = ?";
+        int lemmaId = 0;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(checkQuery);
+            preparedStatement.setString(1, lemma.getLemma());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                lemmaId = resultSet.getInt("id");
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lemmaId;
+    }
+
+    private int getIndexIdFromTableIndex(Page page, Lemma lemma) {
+        java.sql.Connection connection = ConnectionManager.getConnection();
+        String getQuery = "SELECT id FROM search_engine.index WHERE page_id = ? AND lemma_id = ?";
+        int indexId = 0;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(getQuery);
+            preparedStatement.setInt(1, getPageIdFromTablePage(page));
+            preparedStatement.setInt(2, getLemmaIdFromTableLemma(lemma));
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                indexId = resultSet.getInt("id");
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return indexId;
+    }
+
+    private void updateRankInTableIndex(Page page, Lemma lemma) {
+        java.sql.Connection connection = ConnectionManager.getConnection();
+        String updateQuery = "UPDATE search_engine.index SET `rank` = ? WHERE id = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(updateQuery);
+            preparedStatement.setFloat(1, (float) (getRankFromTableIndex(page, lemma) + 0.8 * lemma.getFrequency()));
+            preparedStatement.setInt(2, getIndexIdFromTableIndex(page, lemma));
+            preparedStatement.executeUpdate();
+
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private float getRankFromTableIndex(Page page, Lemma lemma) {
+        java.sql.Connection connection = ConnectionManager.getConnection();
+        String getQuery = "SELECT `rank` FROM search_engine.index WHERE page_id = ? AND lemma_id = ?";
+        float rank = 0;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(getQuery);
+            preparedStatement.setInt(1, getPageIdFromTablePage(page));
+            preparedStatement.setInt(2, getLemmaIdFromTableLemma(lemma));
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                rank = resultSet.getFloat("rank");
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rank;
+    }
+
+    private void insertIntoTableLemma(Set<Lemma> lemmas) {
+        for (Lemma lemma : lemmas) {
+            if (!uniqueLemmas.contains(lemma.getLemma())) {
+                uniqueLemmas.add(lemma.getLemma());
+                insertUniqueLemmaIntoTableLemma(lemma);
+            } else {
+                updateFrequencyOfLemmaInTableLemma(lemma);
+            }
+        }
+    }
+
+    private void insertUniqueLemmaIntoTableLemma(Lemma lemma) {
+        java.sql.Connection connection = ConnectionManager.getConnection();
+        if (!lemma.getLemma().equals(getLemmaWordFromTableLemma(lemma))) {
+            String insertQuery = "INSERT INTO search_engine.lemma (lemma, frequency, site_id) VALUES (?, ?, ?)";
+            try {
+                PreparedStatement preparedStatement = connection.prepareStatement(insertQuery);
+                preparedStatement.setString(1, lemma.getLemma());
+                preparedStatement.setInt(2, 1);
+                preparedStatement.setInt(3, getSiteIdFromTableSite(getUrlSite()));
+                preparedStatement.executeUpdate();
+
+                preparedStatement.close();
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void updateFrequencyOfLemmaInTableLemma(Lemma lemma) {
+        java.sql.Connection connection = ConnectionManager.getConnection();
+        String updateQuery = "UPDATE search_engine.lemma SET frequency = ? WHERE lemma = ?";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(updateQuery);
+            preparedStatement.setInt(1, getLemmaFrequencyFromTableLemma(lemma) + 1);
+            preparedStatement.setString(2, lemma.getLemma());
+            preparedStatement.executeUpdate();
+
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getLemmaWordFromTableLemma(Lemma lemma) {
+        java.sql.Connection connection = ConnectionManager.getConnection();
+        String checkQuery = "SELECT lemma FROM search_engine.lemma WHERE lemma = ?";
+        String lemmaWord = "";
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(checkQuery);
+            preparedStatement.setString(1, lemma.getLemma());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                lemmaWord = resultSet.getString("lemma");
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lemmaWord;
+    }
+
+    private int getSiteIdFromTableSite(String urlSite) {
+        java.sql.Connection connection = ConnectionManager.getConnection();
+        String getQuery = "SELECT id FROM search_engine.site WHERE url = ?";
+        int siteId = 0;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(getQuery);
+            preparedStatement.setString(1, urlSite);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                siteId = resultSet.getInt("id");
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return siteId;
+    }
+
+    private int getLemmaFrequencyFromTableLemma(Lemma lemma) {
+        java.sql.Connection connection = ConnectionManager.getConnection();
+        String getQuery = "SELECT frequency FROM search_engine.lemma WHERE lemma = ?";
+        int frequency = 0;
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(getQuery);
+            preparedStatement.setString(1, lemma.getLemma());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                frequency = resultSet.getInt("frequency");
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return frequency;
     }
 
     private String getPathFromTablePage(Page page) {
@@ -119,6 +374,14 @@ public class WebPageIndexingSystem extends RecursiveTask<Set<Page>> {
                 e.printStackTrace();
             }
         }
+    }
+
+    private String getTextFromTitleOfPage(Document document) {
+        return document.select("title").text();
+    }
+
+    private String getTextFromBodyOfPage(Document document) {
+        return document.select("body").text();
     }
 
     private boolean isValidUrl(String url) {
